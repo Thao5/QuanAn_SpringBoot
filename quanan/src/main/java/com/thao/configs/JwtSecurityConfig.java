a77f61e9127b17af70bf2c2d2fd7d305b8eb0a85
quocthao9899@gmail.com <quocthao9899@gmail.com>
2023-10-12 13:19:07 +0700

    fixed jwt

--- quanan/src/main/java/com/thao/configs/JwtSecurityConfig.java
+++ quanan/src/main/java/com/thao/configs/JwtSecurityConfig.java
@@ -152,7 +152,8 @@ public class JwtSecurityConfig {
                  new AntPathRequestMatcher("/api/current-user/"),
                  new AntPathRequestMatcher("/api/pay/"),
                  new AntPathRequestMatcher("/api/payoffline/"),
-                 new AntPathRequestMatcher("/api/datban/")).permitAll()
+                 new AntPathRequestMatcher("/api/datban/"),
+                 new AntPathRequestMatcher("/api/ban/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/api/**", "GET")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
                         .requestMatchers(new AntPathRequestMatcher("/api/**", "POST")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
                         .requestMatchers(new AntPathRequestMatcher("/api/**", "DELETE")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
