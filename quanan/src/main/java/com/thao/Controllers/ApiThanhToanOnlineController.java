f537f9ef92b3938b5350c2f68a840d6af0498826
quocthao9899@gmail.com <quocthao9899@gmail.com>
2024-02-09 03:15:50 +0700

    update online payment

---
+++ quanan/src/main/java/com/thao/Controllers/ApiThanhToanOnlineController.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.thao.Controllers;
+
+import com.thao.configs.JwtSecurityConfig;
+import com.thao.pojo.PaymentRes;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Chung Vu
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiThanhToanOnlineController {
+    @GetMapping("/create_payment/")
+    public ResponseEntity<?> createPayment(HttpServletRequest req) throws UnsupportedEncodingException{
+        String orderType = "other";
+//        long amount = Integer.parseInt(req.getParameter("amount"))*100;
+//        String bankCode = req.getParameter("bankCode");
+        
+        String vnp_TxnRef = JwtSecurityConfig.getRandomNumber(8);
+        String vnp_IpAddr = JwtSecurityConfig.getIpAddress(req);
+        
+        long amount = 100000*100;
+
+        String vnp_TmnCode = JwtSecurityConfig.vnp_TmnCode;
+        
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", JwtSecurityConfig.vnp_Version);
+        vnp_Params.put("vnp_Command", JwtSecurityConfig.vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+        vnp_Params.put("vnp_BankCode", "NCB");
+        
+//        if (bankCode != null && !bankCode.isEmpty()) {
+//            vnp_Params.put("vnp_BankCode", bankCode);
+//        }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+        vnp_Params.put("vnp_Locale", "vn");
+
+//        String locate = req.getParameter("language");
+//        if (locate != null && !locate.isEmpty()) {
+//            vnp_Params.put("vnp_Locale", locate);
+//        } else {
+//            vnp_Params.put("vnp_Locale", "vn");
+//        }
+        vnp_Params.put("vnp_ReturnUrl", JwtSecurityConfig.vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+        
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+        
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = JwtSecurityConfig.hmacSHA512(JwtSecurityConfig.secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = JwtSecurityConfig.vnp_PayUrl + "?" + queryUrl;
+        
+        PaymentRes paymentRes = new PaymentRes();
+        paymentRes.setStatus("OK");
+        paymentRes.setMessage("Successfully");
+        paymentRes.setURL(paymentUrl);
+//        com.google.gson.JsonObject job = new JsonObject();
+//        job.addProperty("code", "00");
+//        job.addProperty("message", "success");
+//        job.addProperty("data", paymentUrl);
+//        Gson gson = new Gson();
+//        resp.getWriter().write(gson.toJson(job));
+
+        return new ResponseEntity<>(paymentRes, HttpStatus.OK);
+    }
+    
+//    @GetMapping("/payment_info/")
+//    public ResponseEntity<?> transaction(@RequestParam(value = "vnp_Amount") String amount,
+//                                         @RequestParam(value = "vnp_BankCode") String bankCode,
+//                                         @RequestParam(value = "vnp_OrderInfo") String orderInfo,
+//                                         @RequestParam(value = "vnp_ResponseCode") String responseCode){
+//        
+//    }
+}
--- quanan/src/main/java/com/thao/configs/JwtSecurityConfig.java
+++ quanan/src/main/java/com/thao/configs/JwtSecurityConfig.java
@@ -7,6 +7,7 @@ package com.thao.configs;
 import com.thao.filters.CustomAccessDeniedHandler;
 import com.thao.filters.JwtAuthenticationTokenFilter;
 import com.thao.filters.RestAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  *
  * @author Chung Vu
@@ -57,6 +71,117 @@ public class JwtSecurityConfig {
     private UserDetailsService userDetailsService;
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
+
+    public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+    public static String vnp_ReturnUrl = "http://localhost:8080/quanan/admin/nguoidung";
+    public static String vnp_TmnCode = "F08ACHP7";
+    public static String secretKey = "KRNOBXWYMVKGUMTTTEDOQSGUORCTVUUQ";
+    public static String vnp_Version = "2.1.0";
+    public static String vnp_Command = "pay";
+    public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
+
+    public static String md5(String message) {
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(message.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (UnsupportedEncodingException ex) {
+            digest = "";
+        } catch (NoSuchAlgorithmException ex) {
+            digest = "";
+        }
+        return digest;
+    }
+
+    public static String Sha256(String message) {
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(message.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (UnsupportedEncodingException ex) {
+            digest = "";
+        } catch (NoSuchAlgorithmException ex) {
+            digest = "";
+        }
+        return digest;
+    }
+
+    //Util for VNPAY
+    public static String hashAllFields(Map fields) {
+        List fieldNames = new ArrayList(fields.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder sb = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) fields.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                sb.append(fieldName);
+                sb.append("=");
+                sb.append(fieldValue);
+            }
+            if (itr.hasNext()) {
+                sb.append("&");
+            }
+        }
+        return hmacSHA512(secretKey, sb.toString());
+    }
+
+    public static String hmacSHA512(final String key, final String data) {
+        try {
+
+            if (key == null || data == null) {
+                throw new NullPointerException();
+            }
+            final Mac hmac512 = Mac.getInstance("HmacSHA512");
+            byte[] hmacKeyBytes = key.getBytes();
+            final SecretKeySpec secretKey = new SecretKeySpec(hmacKeyBytes, "HmacSHA512");
+            hmac512.init(secretKey);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            byte[] result = hmac512.doFinal(dataBytes);
+            StringBuilder sb = new StringBuilder(2 * result.length);
+            for (byte b : result) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ipAdress;
+        try {
+            ipAdress = request.getHeader("X-FORWARDED-FOR");
+            if (ipAdress == null) {
+                ipAdress = request.getRemoteAddr();
+            }
+        } catch (Exception e) {
+            ipAdress = "Invalid IP:" + e.getMessage();
+        }
+        return ipAdress;
+    }
+
+    public static String getRandomNumber(int len) {
+        Random rnd = new Random();
+        String chars = "0123456789";
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
 
     @Bean
     public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() throws Exception {
@@ -87,13 +212,12 @@ public class JwtSecurityConfig {
     public CustomAccessDeniedHandler customAccessDeniedHandler() {
         return new CustomAccessDeniedHandler();
     }
-    
+
 //    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer() {
 //        return (web) -> web.ignoring()
 //            .requestMatchers(new AntPathRequestMatcher("/api/**"));
 //    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.formLogin()
@@ -114,12 +238,10 @@ public class JwtSecurityConfig {
 //        http.csrf().disable();
         // Disable crsf cho đường dẫn /rest/**
 //        http.csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/api/**")));
-
 //        http.authorizeHttpRequests(rmr -> rmr.requestMatchers(new AntPathRequestMatcher("/api/login/"),
 //                 new AntPathRequestMatcher("/api/food/"),
 //                 new AntPathRequestMatcher("/api/cates/"),
 //                 new AntPathRequestMatcher("/api/dangky/")).permitAll()).csrf(csrf -> csrf.disable());
-
 //        http.securityMatcher(new AntPathRequestMatcher("/api/**"))
 //                .httpBasic(b -> {
 //            try {
@@ -142,29 +264,28 @@ public class JwtSecurityConfig {
 //                    }
 //                }
 //                );
-
         http.userDetailsService(userDetailsService).authorizeHttpRequests(rmr -> {
             try {
                 rmr.requestMatchers(new AntPathRequestMatcher("/api/login/"),
-                 new AntPathRequestMatcher("/api/food/"),
-                 new AntPathRequestMatcher("/api/cates/"),
-                 new AntPathRequestMatcher("/api/dangky/"),
-                 new AntPathRequestMatcher("/api/current-user/"),
-                 new AntPathRequestMatcher("/api/pay/"),
-                 new AntPathRequestMatcher("/api/payoffline/"),
-                 new AntPathRequestMatcher("/api/datban/"),
-                 new AntPathRequestMatcher("/api/ban/**"),
-                 new AntPathRequestMatcher("/api/comments/**"),
-                 new AntPathRequestMatcher("/api/stores/**"),
-                 new AntPathRequestMatcher("/api/thongtinban/**"),
-                 new AntPathRequestMatcher("/api/quenmatkhau/**"),
-                 new AntPathRequestMatcher("/api/doimatkhau/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/api/**", "GET")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
+                        new AntPathRequestMatcher("/api/food/"),
+                        new AntPathRequestMatcher("/api/cates/"),
+                        new AntPathRequestMatcher("/api/dangky/"),
+                        new AntPathRequestMatcher("/api/current-user/"),
+                        new AntPathRequestMatcher("/api/pay/"),
+                        new AntPathRequestMatcher("/api/payoffline/"),
+                        new AntPathRequestMatcher("/api/datban/"),
+                        new AntPathRequestMatcher("/api/ban/**"),
+                        new AntPathRequestMatcher("/api/comments/**"),
+                        new AntPathRequestMatcher("/api/stores/**"),
+                        new AntPathRequestMatcher("/api/thongtinban/**"),
+                        new AntPathRequestMatcher("/api/quenmatkhau/**"),
+                        new AntPathRequestMatcher("/api/doimatkhau/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/**", "GET")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
                         .requestMatchers(new AntPathRequestMatcher("/api/**", "POST")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
                         .requestMatchers(new AntPathRequestMatcher("/api/**", "DELETE")).hasAnyAuthority("ADMIN", "OWNER", "CUSTOMER")
                         .and()
                         .httpBasic(b -> b.authenticationEntryPoint(restServicesEntryPoint()))
-//                        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        //                        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class)
                         .exceptionHandling(e -> e.accessDeniedHandler(customAccessDeniedHandler())).csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/api/**")));
             } catch (Exception ex) {
